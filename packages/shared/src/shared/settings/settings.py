45fdcf084550e84c42bb39a5825d94dae88461d8
Ajay Kanki <222144676+ajaykanki@users.noreply.github.com>
2025-11-25 23:01:02 +0530

    feat(shared): add db url warning post settings model init

--- packages/shared/src/shared/settings/settings.py
+++ packages/shared/src/shared/settings/settings.py
@@ -1,3 +1,4 @@
+from shared.logger import log
 from shared.utils import ENV, DEVELOPMENT, KEYRING_SERVICE_NAME, get_env_file
 from pydantic_settings import BaseSettings, SettingsConfigDict
 from pydantic import PostgresDsn
@@ -12,3 +13,9 @@ class Settings(BaseSettings):
     @property
     def is_dev(self) -> bool:
         return self.env in DEVELOPMENT
+
+    def model_post_init(self, context):
+        if self.database_url is None:
+            log.warning("DATABASE_URL is not set in environment variables")
+
+        return super().model_post_init(context)
